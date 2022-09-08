64f449473d72cbaf19f6837a8ac43c577c8d7089
jonno293 <65672029+jonno293@users.noreply.github.com>
2022-09-08 05:57:39 -0700

    Update SwampControlsample.py

--- SwampControlsample.py
+++ SwampControlsample.py
@@ -5,7 +5,7 @@ import time
 import csv
 import pandas as pd
 import DHT11
-import masterstat
+import masterstat 
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
@@ -19,51 +19,12 @@ temp = 75
 overrideTime = 2.5 #hours
 setTemp = 75
 
-def updateConfig():
+def updateConfig(): #config.csv contains temp schedule and other settings
     configData = pd.read_csv('config.csv',index_col=0,skiprows=25,header=None,usecols=[0,1],names=['name','setting'])
     scheduleData = pd.read_csv('config.csv',index_col=0,nrows=24,header=0)
     print(configData)
     # print(scheduleData)
 
-    totalOn = sum(switches)
-    remainingTime = 0.887-totalOn*0.005
-    for x in range(3):
-        GPIO.output(8,1)
-        sleep(0.031)
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(0.011)
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(0.011)
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(0.011)
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(0.011)
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(fanHi+0.005*switches[1])
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(fanLo+0.005*switches[2])
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(purgePump+0.005*switches[3])
-        GPIO.output(8,0)
-        sleep(0.002)
-        GPIO.output(8,1)
-        sleep(pump+0.005*switches[4])
-        GPIO.output(8,0)
-        sleep(remainingTime)
 
 def overrideTemp(newTemp,overrideTime):
     global SetTemp
